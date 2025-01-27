01a2b061c601c908ec38c61d144fc8b84ccfc99c
Jean Silva <jean-vs@hotmail.com>
2025-01-27 20:30:10 -0300

    Jean

--- lambda_src/inferLLM/lambda_function.py
+++ lambda_src/inferLLM/lambda_function.py
@@ -16,14 +16,15 @@ s3 = boto3.client('s3')
 def lambda_handler(event, context):
     bucket_name = 'awenaife-llm-infer-prompt'
     file_key = 'prompt.txt'
-    print('Event-----> ', event)
-    try:
-        s3_response = s3.get_object(Bucket=bucket_name, Key=file_key)
-        file_content = s3_response['Body'].read().decode('utf-8')
 
+    try:
         raw_body = event.get('body', '')
         json_body = json.loads(raw_body)
         input_value = json_body['input']
+
+        s3_response = s3.get_object(Bucket=bucket_name, Key=file_key)
+        file_content = s3_response['Body'].read().decode('utf-8')
+
         input_value = file_content + input_value
         body = "{\"inputText\":\"" + input_value + "\",\"textGenerationConfig\":{\"maxTokenCount\":8192,\"stopSequences\":[],\"temperature\":0,\"topP\":1}}"
 
