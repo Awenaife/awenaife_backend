d2a184d1bbd84fbf6ee347541d53c2e213aba465
Jean Silva <jean-vs@hotmail.com>
2025-01-27 20:33:18 -0300

    Jean

--- lambda_src/inferLLM/lambda_function.py
+++ lambda_src/inferLLM/lambda_function.py
@@ -1,6 +1,6 @@
 import json
 import boto3
-
+#teste 1
 bedrock = boto3.client(
     service_name='bedrock',
     region_name='us-east-1'
@@ -11,39 +11,23 @@ bedrock_runtime = boto3.client(
     region_name='us-east-1'
 )
 
-s3 = boto3.client('s3')
-
 def lambda_handler(event, context):
-    bucket_name = 'awenaife-llm-infer-prompt'
-    file_key = 'prompt.txt'
+    raw_body = event.get('body', '')
+    json_body = json.loads(raw_body)
+    input_value = json_body['input']
+    body = "{\"inputText\":\"" + input_value + "\",\"textGenerationConfig\":{\"maxTokenCount\":8192,\"stopSequences\":[],\"temperature\":0,\"topP\":1}}"
 
-    try:
-        raw_body = event.get('body', '')
-        json_body = json.loads(raw_body)
-        input_value = json_body['input']
+    response = bedrock_runtime.invoke_model(
+        body=body,
+        modelId='amazon.titan-text-express-v1',
+        accept='application/json',
+        contentType='application/json'
+    )
 
-        s3_response = s3.get_object(Bucket=bucket_name, Key=file_key)
-        file_content = s3_response['Body'].read().decode('utf-8')
+    response_body = json.loads(response.get('body').read())
 
-        input_value = file_content + input_value
-        body = "{\"inputText\":\"" + input_value + "\",\"textGenerationConfig\":{\"maxTokenCount\":8192,\"stopSequences\":[],\"temperature\":0,\"topP\":1}}"
-
-        response = bedrock_runtime.invoke_model(
-            body=body,
-            modelId='amazon.titan-text-express-v1',
-            accept='application/json',
-            contentType='application/json'
-        )
-
-        response_body = json.loads(response.get('body').read())
-
-        return {
-            'statusCode': 200,
-            'body': json.dumps({"Answer": response_body})
-        }
-    except Exception as e:
-        print(f"Error: {str(e)}")
-        return {
-            'statusCode': 500,
-            'body': json.dumps({"Error": str(e)})
-        }
+    print(response_body)
+    return {
+        'statusCode': 200,
+        'body': json.dumps({"Answer": response_body})
+    }
