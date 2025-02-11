817f1a8b96b5b0a7e43142bcfd8f6d9054d99741
Jean Silva <jean-vs@hotmail.com>
2025-02-11 18:46:36 -0300

    GPT40mini_API

--- lambda_src/inferLLM/lambda_function.py
+++ lambda_src/inferLLM/lambda_function.py
@@ -8,13 +8,16 @@ s3 = boto3.client('s3')
 def lambda_handler(event, context):
     bucket_name = os.environ.get("BUCKET_NAME") 
     prompt_file_key = os.environ.get("PROMPT_FILE") 
-    openai_api_key  = os.environ.get("OPENAI_API_KEY")
-    client = OpenAI(api_key=openai_api_key)
+    apikey_file_key  = os.environ.get("APIKEY_FILE")
 
     try:
         s3_response_p = s3.get_object(Bucket=bucket_name, Key=prompt_file_key)
         prompt = s3_response_p['Body'].read().decode('utf-8')
-        
+
+        s3_response_a = s3.get_object(Bucket=bucket_name, Key=apikey_file_key)
+        apikey = s3_response_a['Body'].read().decode('utf-8')
+        client = OpenAI(api_key=apikey)
+
         parsed_body = json.loads(event['body'])
         input_value = parsed_body['input']
         
