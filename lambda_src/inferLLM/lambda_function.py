9068fd2414d5e49fdddce71bf201cc7de4551392
Jean Silva <jean-vs@hotmail.com>
2025-02-12 13:28:01 -0300

    GPT40mini_API

--- lambda_src/inferLLM/lambda_function.py
+++ lambda_src/inferLLM/lambda_function.py
@@ -1,7 +1,7 @@
 import json
 import boto3
 import os
-from openai import OpenAI
+import urllib.request
 
 s3 = boto3.client('s3')
 
@@ -9,27 +9,48 @@ def lambda_handler(event, context):
     bucket_name = os.environ.get("BUCKET_NAME") 
     prompt_file_key = os.environ.get("PROMPT_FILE") 
     apikey_file_key  = os.environ.get("APIKEY_FILE")
+    apiurl = os.environ.get("API_URL")
 
+    s3_response_p = s3.get_object(Bucket=bucket_name, Key=prompt_file_key)
+    prompt = s3_response_p['Body'].read().decode('utf-8')
+
+    s3_response_a = s3.get_object(Bucket=bucket_name, Key=apikey_file_key)
+    apikey = s3_response_a['Body'].read().decode('utf-8')
+
+    parsed_body = json.loads(event['body'])
+    input_value = parsed_body['input']
+
+    headers = {
+        "Authorization": f"Bearer {apikey}",
+        "Content-Type": "application/json"
+    }
+        
+    data = {
+        "model": "gpt-4o-mini",
+        "messages": [
+            {"role": "system", "content": prompt},
+            {"role": "user", "content": input_value}
+        ],
+        "temperature": 0.7
+    }
+        
+    json_data = json.dumps(data).encode("utf-8")
+    request = urllib.request.Request(
+        apiurl,
+        data=json_data,
+        headers={
+            "Authorization": f"Bearer {apikey}",
+            "Content-Type": "application/json"
+        },
+        method="POST"
+    )
+    
     try:
-        s3_response_p = s3.get_object(Bucket=bucket_name, Key=prompt_file_key)
-        prompt = s3_response_p['Body'].read().decode('utf-8')
+        with urllib.request.urlopen(request) as response:
+            response_data = response.read()
+            result = json.loads(response_data)
+            response_text = result["choices"][0]["message"]["content"]
 
-        s3_response_a = s3.get_object(Bucket=bucket_name, Key=apikey_file_key)
-        apikey = s3_response_a['Body'].read().decode('utf-8')
-        client = OpenAI(api_key=apikey)
-
-        parsed_body = json.loads(event['body'])
-        input_value = parsed_body['input']
-        
-        response_body = client.chat.completions.create(
-            model="gpt-4o-mini",
-            messages = [
-                {"role": "system", "content": prompt},
-                {"role": "user", "content": input_value}
-            ]
-        )
-        response_text = response_body.choices[0].message.content
-        
         return {
             'statusCode': 200,
             'body': json.dumps({"Answer": response_text})
